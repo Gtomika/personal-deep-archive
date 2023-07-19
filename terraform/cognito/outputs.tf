ab7269a58572a8dcb67dd7fba251e1db1b13b383
tamas-gaspar <tamas.gaspar@prt.manomano.com>
2023-07-19 10:15:54 +0200

    Output user pool id

--- terraform/cognito/outputs.tf
+++ terraform/cognito/outputs.tf
@@ -1,3 +1,7 @@
 output "cognito_user_pool_arn" {
   value = aws_cognito_user_pool.personal_archive_user_pool.arn
 }
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.personal_archive_user_pool.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "archive_bucket_arn" {
   value = module.archive_bucket.archive_storage_bucket_arn
 }
+
+output "user_pool_id" {
+  value = module.cognito_setup.cognito_user_pool_id
+}
